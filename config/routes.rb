55bffcb165181d4c7b5adcdd70daea08e44d1b4e
Jeremy Chiang <jeremycpc2@gmail.com>
2018-04-13 18:10:53 +1000

    Added destroy buttons

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'cocktails#index'
-  resources :cocktails, only: [:new, :create, :show] do
+  resources :cocktails, only: [:new, :create, :show, :destroy] do
     resources :doses, only: [:new, :create]
   end
   resources :doses, only: [:destroy]
